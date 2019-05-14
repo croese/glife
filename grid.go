82828f62dec725282f907662932fbd1ab3ac34d9
Christian Roese <croese@gmail.com>
2019-05-14 18:38:09 -0500

    Adds tests and prod code for multiple cells

--- grid.go
+++ grid.go
@@ -1,12 +1,22 @@
 package glife
 
 type Grid struct {
+	rows, cols int
+	cells      []bool
 }
 
 func NewGrid(rows, cols int) *Grid {
-	return &Grid{}
+	return &Grid{
+		rows:  rows,
+		cols:  cols,
+		cells: make([]bool, rows*cols),
+	}
 }
 
 func (g *Grid) IsAlive(row, col int) bool {
-	return false
+	return g.cells[row*g.cols+col]
+}
+
+func (g *Grid) MarkAlive(row, col int) {
+	g.cells[row*g.cols+col] = true
 }
--- grid_test.go
+++ grid_test.go
@@ -2,10 +2,48 @@ package glife
 
 import "testing"
 
+func expectDead(g *Grid, row, col int, t *testing.T) {
+	if g.IsAlive(row, col) {
+		t.Errorf("expected cell to be dead")
+	}
+}
+
+func expectAlive(g *Grid, row, col int, t *testing.T) {
+	if !g.IsAlive(row, col) {
+		t.Errorf("expected cell to be alive")
+	}
+}
+
 func TestThatNewGridHasAllDeadCells(t *testing.T) {
 	g := NewGrid(1, 1)
 
-	if g.IsAlive(1, 1) {
-		t.Errorf("expected cell at (1,1) to be dead")
-	}
+	expectDead(g, 0, 0, t)
+}
+
+func TestThatACellCanBeMarkedAlive(t *testing.T) {
+	g := NewGrid(1, 1)
+
+	g.MarkAlive(0, 0)
+
+	expectAlive(g, 0, 0, t)
+}
+
+func TestThatCellsCanHaveDifferentStates(t *testing.T) {
+	g := NewGrid(1, 2)
+
+	g.MarkAlive(0, 0)
+
+	expectAlive(g, 0, 0, t)
+	expectDead(g, 0, 1, t)
+}
+
+func TestGridWithSecondRow(t *testing.T) {
+	g := NewGrid(2, 2)
+
+	g.MarkAlive(1, 0)
+
+	expectDead(g, 0, 0, t)
+	expectDead(g, 0, 1, t)
+	expectAlive(g, 1, 0, t)
+	expectDead(g, 1, 1, t)
 }
